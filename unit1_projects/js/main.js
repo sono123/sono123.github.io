13e55080f0a2925d9f3dfaa97c1ebd2af19324c1
steveono <steveono@gmail.com>
2014-07-26 23:34:24 -0400

    new homepage

---
+++ unit1_projects/js/main.js
@@ -0,0 +1,89 @@
+
+$('img#ruby_logo').on('mouseenter', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/ruby_logo_hover.png")
+})
+
+$('img#ruby_logo').on('mouseleave', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/ruby_logo.png")
+})
+
+
+$('img#js_logo').on('mouseenter', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/js_logo_hover.png")
+})
+
+$('img#js_logo').on('mouseleave', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/js_logo.png")
+})
+
+$('img#html_logo').on('mouseenter', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/html_logo_hover.png")
+})
+
+$('img#html_logo').on('mouseleave', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/html_logo.png")
+})
+
+$('img#css_logo').on('mouseenter', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/css_logo_hover.png")
+})
+
+$('img#css_logo').on('mouseleave', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/css_logo.png")
+})
+
+$('img#sql_logo').on('mouseenter', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/sql_logo_hover.png")
+})
+
+$('img#sql_logo').on('mouseleave', function(e){
+   e.preventDefault()
+   $(this).attr('src', "../unit1_projects/images/icons/sql_logo.png")
+})
+
+// $('.summary').click(function(e){
+// 	e.preventDefault();
+// 	$(this).next().slideToggle();
+// });
+
+// $('.closedivbutton').click(function(e){
+// 	e.preventDefault();
+// 	$(this).closest('.showmessage').slideUp();
+// });
+
+// $('.closebutton').click(function(e){
+// 	e.preventDefault();
+// 	$(this).closest('.showmessage').slideUp();
+// });
+
+// $('.menubutton').click(function(){
+// 	$('.mainmenu').toggleClass('active');
+// });
+
+// $('.checkbutton').click(function(){
+// 	$('.checkmenu').toggleClass('active');
+// });
+
+// $('.username').click(function(){
+// 	$('.usernamemenu').toggleClass('active');
+// });
+
+
+
+
+
+
+
+
+
+
+
